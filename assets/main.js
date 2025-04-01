9a5fcb96aa9cab7af9dd35197f4a6fce911be041
kmcgehee <kotomimcgehee@gmail.com>
2025-04-01 13:18:33 -0500

    removed Key Services bg change function, updated below hero section bg to secondary color from primary color.

--- assets/main.js
+++ assets/main.js
@@ -10,17 +10,3 @@ document.addEventListener("DOMContentLoaded", function () {
     });
 });
 
-document.addEventListener("DOMContentLoaded", function () {
-    const servicesSection = document.querySelector(".services");
-    const cards = document.querySelectorAll(".services .card");
-
-    cards.forEach((card, index) => {
-        card.addEventListener("mouseenter", function () {
-            servicesSection.classList.add(`service0${index + 1}`);
-        });
-
-        card.addEventListener("mouseleave", function () {
-            servicesSection.classList.remove(`service0${index + 1}`);
-        });
-    });
-});
